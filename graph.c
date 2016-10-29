5037154c781745c6b135e318dea16259fb6bb11e
Panagiotis Fotopoulos <panos.foto@hotmail.com>
2016-10-29 23:40:55 +0300

    create graph, appropriate error codes

--- graph.c
+++ graph.c
@@ -15,20 +15,51 @@ typedef struct graph
 
 rcode gCreateGraph(pGraph *g)
 {
-
+	if (g == NULL) return GRAPH_NULL_POINTER_PROVIDED;
+	if ((*g = malloc(sizeof(graph))) == NULL)
+	{
+		return GRAPH_CREATION_BASIC_STRUCT_MALLOC_FAIL;
+	}
+	if (((*g)->inIndex = createNodeIndex()) == NULL)
+	{
+		free(*g);
+		return GRAPH_CREATION_INDEX_MALLOC_FAIL;
+	}
+	if (((*g)->outIndex = createNodeIndex()) == NULL);
+	{
+		free((*g)->inIndex);
+		free(*g);
+		return GRAPH_CREATION_INDEX_MALLOC_FAIL;
+	}
+	if (((*g)->inBuffer = createBuffer()) == NULL)
+	{
+		free((*g)->outIndex);
+		free((*g)->inIndex);
+		free(*g);
+		return GRAPH_CREATION_BUFFER_MALLOC_FAIL;
+	}
+	if (((*g)->outBuffer = createBuffer()) == NULL)
+	{
+		free((*g)->inBuffer);
+		free((*g)->outIndex);
+		free((*g)->inIndex);
+		free(*g);
+		return GRAPH_CREATION_BUFFER_MALLOC_FAIL;
+	}
+	return OK_SUCCESS;
 }
 
 rcode gAddNode(pGraph g, graphNode from, graphNode to)
 {
-
+	return OK_SUCCESS;
 }
 
 rcode gFindShortestPath(pGraph g, graphNode from, graphNode to/*, &solution*/)
 {
-
+	return OK_SUCCESS;
 }
 
 rcode gDestroyGraph(pGraph g)
 {
-
+	return OK_SUCCESS;
 }
