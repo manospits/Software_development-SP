7ff645a8b8ffe47b296dbe023226e3591e03ea44
Panagiotis Fotopoulos <panos.foto@hotmail.com>
2016-10-31 23:30:27 +0200

    (added main to the commit, forgot in previous one)

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#include "graph.h"
+#include "error.h"
+
+int main(int argc, char *argv[])
+{
+	int choice=-1;
+    int node1, node2, ret_val;
+    pGraph testgraph = gCreateGraph();
+    while(choice!=0){
+        printf("1 add node\n2 query\n3 print\n");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1:
+                scanf("%d %d",&node1, &node2);
+                gAddEdge(testgraph,node1, node2);
+                break;
+            case 2:
+                scanf("%d %d",&node1, &node2);
+                ret_val = gFindShortestPath(testgraph, node1, node2, BFS);
+                if (ret_val < 0) print_error();
+                else printf("Shortest path length: %d\n", ret_val);
+                break;
+            case 3:
+                gPrintGraph(testgraph);
+                break;
+        }
+    }
+	return 0;
+}
