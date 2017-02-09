65ca17033a57b1dc5053fd51efe44b498fcff457
manospits <mnls95@gmail.com>
2017-02-09 11:06:46 +0200

    added in sources main files and unused generic list

---
+++ sources/mains_and_utilities/genericlist/list.h
@@ -0,0 +1,30 @@
+#ifndef _LIST_
+#define _LIST_
+#define QUEUE_INIT_SIZE 500000
+#include "error.h"
+#include <stdint.h>
+#include <stdlib.h>
+
+
+typedef struct ghead * gphead;
+
+//CREATION
+gphead gcr_list(size_t data_size,rcode(*data_copy)(char*,void*),int(*data_cmp)(void*,void*));
+//DESTRUCTION
+rcode gds_list(gphead list_to_destroy);
+rcode gempty_list(gphead list_to_empty);
+//MODIFY
+rcode ginsert_back(gphead listh,void * data);          //inserts node in back
+rcode gpop_front(gphead listh);                         //deletes first node in list
+rcode gpop_back(gphead listh);
+
+
+//ACCESS
+int gget_size(const gphead listh);                  //get number of elements
+void * gget_data(void *data);                     //returns data
+void * gpeek(const gphead listh);                 //returns list's first int
+rcode int_copy(char*a,void*b);
+int int_cmp(void*a,void*b);
+
+
+#endif
